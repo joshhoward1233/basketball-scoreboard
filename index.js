7d8a6b401d0d8dd090ecafc2e3c4ece2fe07d1e9
joshhoward1233 <joshhoward1233@hotmail.com>
2022-09-23 01:38:24 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+let scoreHomeEl = document.getElementById("score-home-el")
+let scoreGuestEl = document.getElementById("score-guest-el")
+
+let countHome = 0
+let countGuest = 0
+
+// Home Score Count
+
+function freethrowHome() {
+    countHome += 1 // countHome = countHome + 1
+    scoreHomeEl.textContent = countHome
+}
+
+function midrangeHome() {
+    countHome += 2 // countHome = countHome + 2
+    scoreHomeEl.textContent = countHome
+}
+
+function longthreeHome() {
+    countHome += 3 // countHome = countHome + 3
+    scoreHomeEl.textContent = countHome
+}
+
+// Guest Score Count
+
+function freethrowGuest() {
+    countGuest += 1 // countGuest = countGuest + 1
+    scoreGuestEl.textContent = countGuest
+}
+
+function midrangeGuest() {
+    countGuest += 2 // countGuest = countGuest + 2
+    scoreGuestEl.textContent = countGuest
+}
+
+function longthreeGuest() {
+    countGuest += 3 // countGuest = countGuest + 3
+    scoreGuestEl.textContent = countGuest
+}
